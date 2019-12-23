2f9bc41182fa249f0bbc68a4a19167475a492e66
imenghu <cxm2972@163.com>
2019-12-23 16:39:22 +0800

    任一个英文的纯文本文件，统计其中的单词出现的个数。
    Signed-off-by: imenghu <cxm2972@163.com>

--- words_counts.py
+++ words_counts.py
@@ -1,5 +1,35 @@
-def ReadFile(file):
-    '''将生成的值保存到文本文件中'''
-    with open('Result Key.txt', 'rt') as f:
-        f.read()
-        print(f)
+def ReadFile():
+    '''读取文件名称并判断英文的字符个数'''
+    with open(r'files\\wordcounts.txt', 'rt', encoding='utf-8') as f:
+        words = f.readlines()
+    count = 0
+    for wordline in words:
+        # for word in wordline.strip().split(" "):
+        #     print(word)
+        #     count+=1
+
+        temp = wordline.strip().split(" ")
+        print(temp)
+        count += len(temp)
+
+    print(count)
+
+
+if __name__ == '__main__':
+    ReadFile()
+
+
+#
+# def word_counts(inputfile):
+# 	"""
+# 	"""
+# 	if os.path.isfile(inputfile) != True:
+# 		print "inputfile not exists"
+# 		sys.exit()
+# 	word_count = 0
+# 	words = open(inputfile, "r").readlines()
+# 	for word in words:
+# 		print ("word: %s" %word)
+# 		temp = word.strip().split(' ')
+# 		word_count += len(temp)
+# 	print ("word count:", word_count)
